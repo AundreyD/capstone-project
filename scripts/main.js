d80db8589c0c1d9e5f995a1de07620275331e443
frozegnome <pcaron@att.net>
2015-11-28 15:42:44 -0500

    Build changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-console.log("'Allo 'Allo!");
+!function(e){"use strict";var n="http://api.bandsintown.com/artists/Phantogram/events.json?api_version=2.0&app_id=capstone_proj_bc",t=e("#bit-feed"),a=Handlebars.compile(document.getElementById("bit-template").innerHTML);Handlebars.registerHelper("moment",function(e){return window.moment?moment(e).format("lll"):e});var o=function(){e.ajax({url:n,type:"GET",dataType:"jsonp",success:function(e){t.append(a(e))}})};o()}(jQuery);
